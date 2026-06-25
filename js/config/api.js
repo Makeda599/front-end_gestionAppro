df9afffa2866e14d1e80e67e749903aaf86c377f
makeda <adm670in@gmail.com>
2026-06-25 13:03:35 +0000

    lien déploiement

--- js/config/api.js
+++ js/config/api.js
@@ -1,4 +1,6 @@
-export const API_BASE_URL = "http://localhost:3000"
+// export const API_BASE_URL = "http://localhost:3000"
+export const API_BASE_URL = "https://back-end-gestionappro.onrender.com"
+
 
 export const ENDPOINTS = {
     categories: `${API_BASE_URL}/categories`,
