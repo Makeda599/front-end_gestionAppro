cf6889b1e7e3e09eb0e7ffe820b4343dba1defdb
makeda <adm670in@gmail.com>
2026-06-30 13:44:10 +0000

    api

--- js/config/api.js
+++ js/config/api.js
@@ -4,5 +4,7 @@ export const API_BASE_URL = "https://back-end-gestionappro.onrender.com"
 
 export const ENDPOINTS = {
     categories: `${API_BASE_URL}/categories`,
-    produits: `${API_BASE_URL}/produits` 
+    produits: `${API_BASE_URL}/produits`,
+    users: `${API_BASE_URL}/users`,       
+    fournisseurs: `${API_BASE_URL}/fournisseurs`
 }
